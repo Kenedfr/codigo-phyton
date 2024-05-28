58e80689881f2af7fbb60581ffe79a1441c39e3e
Kenedfr <yuseeeei7@gmail.com>
2024-05-28 18:07:31 -0300

    Add files via upload

---
+++ codigos python/calculadora.py
@@ -0,0 +1,13 @@
+a = 20
+b = 10
+
+soma = a + b
+subtração = a - b
+multiplação = a * b 
+divisão = a / b 
+
+
+print("Resultado da soma: " + str(soma))
+print("Resultado da subtração: " + str(subtração))
+print("Resultado da multiplicação: " + str(multiplação))
+print("Resultado da divisão: " + str(divisão))
